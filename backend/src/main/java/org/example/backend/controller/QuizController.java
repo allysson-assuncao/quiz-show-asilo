96ad7141efbb53c8a83e46219cd03fc26605701d
Kevin Rehem <kevin.rehem@gmail.com>
2025-07-18 12:58:42 -0300

    (FIX) QuizBackendLogicStart [UNTESTED]

---
+++ backend/src/main/java/org/example/backend/controller/QuizController.java
@@ -0,0 +1,11 @@
+package org.example.backend.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/app/quiz")
+public class QuizController {
+}
---
+++ backend/src/main/java/org/example/backend/service/QuizService.java
@@ -0,0 +1,14 @@
+package org.example.backend.service;
+
+import org.example.backend.repository.QuizRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizService {
+
+    private final QuizRepository quizRepository;
+
+    public QuizService(QuizRepository quizRepository) {
+        this.quizRepository = quizRepository;
+    }
+}
