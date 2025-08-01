278b6f89194675762034407126b1dcef98bc8536
antonio-carlosjr <profissional.contatocarlos@gmail.com>
2025-08-01 16:55:27 -0300

    feat - quiz repo

--- backend/src/main/java/org/example/backend/repository/QuizRepository.java
+++ backend/src/main/java/org/example/backend/repository/QuizRepository.java
@@ -12,7 +12,10 @@ import java.util.UUID;
 
 @Repository
 public interface QuizRepository extends JpaRepository<Quiz, UUID> {
+
     Optional<Quiz> findByTitle(String title);
+
+    boolean existsByTitleAndIdNot(String title, UUID id);
 
     @Query("SELECT new org.example.backend.dto.Quiz.SimpleQuizDTO(q.id, q.title, q.description, size(q.questions)) FROM Quiz q ORDER BY q.title")
     List<SimpleQuizDTO> findAllSimple();
